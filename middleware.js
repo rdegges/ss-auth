a4af15204880b2779ae658547f5a85208355ec70
Randall Degges <r@rdegges.com>
2017-05-21 14:23:28 -0700

    Adding the `loginRequired` middleware definition.

--- middleware.js
+++ middleware.js
@@ -4,23 +4,13 @@ const models = require('./models');
 /**
  * A simple authentication middleware for Express.
  *
- * This middleware will load users from session data, and handle all user
- * proxying for convenience.
+ * This middleware checks to see if a user is available in the request. If not,
+ * it will redirect the visitor to the login page.
  */
 module.exports.loginRequired = (req, res, next) => {
-  if (req.session && req.session.user) {
-    models.User.findOne({ email: req.session.user }, 'firstName lastName email', (err, user) => {
-      if (err) {
-        return next(err);
-      }
-
-      if (user) {
-        auth.createUserSession(req, res, user);
-      }
-
-      return next();
-    });
+  if (req.user) {
+    return next();
   }
 
-  next();
+  res.redirect("/login");
 };
