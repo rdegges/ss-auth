3078c665dae73cfdf589190a930dac769f40e55d
Randall Degges <r@rdegges.com>
2017-05-21 14:31:49 -0700

    Adding auth.js helpers.

---
+++ auth.js
@@ -0,0 +1,42 @@
+/**
+ * Given a user object:
+ *
+ *  - Store the user object as a req.user
+ *  - Make the user object available to templates as #{user}
+ *  - Set a session cookie with the user ID
+ *
+ *  @param {Object} req - The http request object.
+ *  @param {Object} res - The http response object.
+ *  @param {Object} user - A user object.
+ */
+module.exports.createUserSession = (req, res, user) => {
+  console.log("Creating User Session:", user._id);
+  req.session.user = user._id;
+};
+
+/**
+ * Load the user object into the request from the session data.
+ *
+ *  @param {Object} req  - The http request object.
+ *  @param {Object} res  - The http response object.
+ *  @param {Object} next - Continue processing the request.
+ */
+module.exports.loadUserFromSession = (req, res, next) => {
+  if (!(req.session && req.session.user)) {
+    return next();
+  }
+
+  models.User.findById(req.session.user, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    // Here is where we store the user object in the current request for
+    // developer usage.  If the user wasn't found, these values will be set to a
+    // non-truthy value, so it won't affect anything.
+    req.user = user;
+    res.locals.user = user;
+
+    next();
+  });
+}
